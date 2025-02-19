f6215c3b2f161ab1e5692b0c18354e447cae6e45
Serena <23511780+SerenaLynas@users.noreply.github.com>
2025-02-18 22:53:06 -0500

    Built with CMSIS

--- sirin-c/src/lib.rs
+++ sirin-c/src/lib.rs
@@ -1,5 +1,13 @@
 #![no_std]
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core::ffi::c_float;
+
+extern "C" {
+    fn test_sin(x: c_float) -> c_float;
+}
+
+pub fn cmsis_dsp_sin(x: f32) -> f32 {
+    unsafe {
+        test_sin(x)
+    }
 }
