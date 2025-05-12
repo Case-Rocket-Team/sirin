90b08bb16304d393d117e4acfeb16d75b8e7ab8d
Serena <23511780+SerenaLynas@users.noreply.github.com>
2025-05-11 22:56:20 -0400

    Use derive_more instead of rolling our own

--- sirin/src/error.rs
+++ sirin/src/error.rs
@@ -1,32 +1,14 @@
+use derive_more::From;
 use sirin_shared::song::{ToSongError, FromSongError};
 use embassy_sync::pubsub::Error as PubSubError;
 use rfm9::Rfm9Error;
 use embassy_usb::driver::EndpointError as USBError;
 
-macro_rules! join_error {
-    (enum $err:ident ($($specialized_err:ident),*)) => {
-        // Every possible Sirin error in one Big Beautiful Enum
-        #[derive(Debug, Clone)]
-        pub enum $err {
-            $(
-                $specialized_err($specialized_err)
-            ),*
-        }
-
-        $(
-            impl From<$specialized_err> for $err {
-                fn from(value: $specialized_err) -> Self {
-                    Self::$specialized_err(value)
-                }
-            }
-        )*
-    };
+#[derive(Debug, From)]
+pub enum SirinError {
+    ToSongError(ToSongError),
+    FromSongError(FromSongError),
+    Rfm9Error(Rfm9Error),
+    UsbError(USBError),
+    PubSubError(PubSubError)
 }
-
-join_error!(enum SirinError (
-    PubSubError,
-    ToSongError,
-    FromSongError,
-    Rfm9Error,
-    USBError
-));
