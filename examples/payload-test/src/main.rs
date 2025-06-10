39e2dcde3bd50f1a707bbc121f47eb8b5d6cd671
Isaac-Porter <isaac.porter144@gmail.com>
2025-06-10 11:16:03 -0500

    payload+airbrakes stuff

--- examples/payload-test/src/main.rs
+++ examples/payload-test/src/main.rs
@@ -7,7 +7,7 @@ use core::{f32::consts::PI, mem::{self, transmute_copy, MaybeUninit}, pin::Pin, 
 use bmp3::{hal::{Bmp3RawData, ReadBmp3, RegErrReg, RegStatus}, Bmp3Readout};
 use defmt::{debug, info, println, Debug2Format};
 use embassy_executor::{task, Executor, Spawner};
-use embassy_stm32::{bind_interrupts, dma::NoDma, gpio::{self, Level, Output, OutputType, Speed}, pac::{gpio::Gpio, metadata::Interrupt, Interrupt::BDMA_CHANNEL0, GPIOA}, peripherals::{self, DMA1_CH0, DMA1_CH1, PD8, PD9, USART3}, time::khz, timer::{simple_pwm::{PwmPin, SimplePwm, SimplePwmChannel}, GeneralInstance4Channel}, usart::{self, Config, Uart}, Peripherals};
+use embassy_stm32::{bind_interrupts, dma::NoDma, gpio::{Level, Output, Speed}, peripherals::{self, DMA1_CH0, DMA1_CH1, PD8, PD9, USART3}, usart::{self, Config, Uart}, Peripherals};
 use embassy_time::{Duration, Instant, Timer, TICK_HZ};
 use embedded_hal_1::spi::ErrorKind;
 use postcard::take_from_bytes;
@@ -15,7 +15,7 @@ use rfm9::{ReadRfm9, Rfm9};
 use sirin_c::update_with_imu;
 use w25qx::W25Q;
 use {defmt_rtt as _, panic_probe as _};
-use sirin::{error::SirinError, flash::Flash, gps::gps_task, io::{broadcast, broadcast_log, flash_io_task, radio_io_task, send_packet, set_usb_broadcasting_enabled, try_receive_packet, usb_input_task, usb_output_task, IN_CHANNEL}, packet::{InPacket, IoChannel, IoPacket, Log, LogEntry, OutPacket, PacketError, Page}, song::{FromSong, SongSize}, spi::SpiDev, state::{Accel, AngularVel, ErrorState, NominalState, Pos, Vel}, subsystems::SirinData, sync::Mutex, time::{duration_since_epoch, set_duration_since_epoch}, uunit::{Gs, Meters, MetersPerSecond2, WithUnits}, Radio, Sirin};
+use sirin::{error::SirinError, flash::Flash, gps::gps_task, io::{broadcast, broadcast_log, flash_io_task, radio_io_task, send_packet, set_usb_broadcasting_enabled, try_receive_packet, usb_input_task, usb_output_task, IN_CHANNEL}, packet::{InPacket, IoChannel, IoPacket, Log, LogEntry, OutPacket, PacketError, Page}, song::{FromSong, SongSize}, spi::SpiDev, state::{Accel, AngularVel, ErrorState, NominalState, Pos, Vel}, subsystems::SirinData, sync::Mutex, time::{duration_since_epoch, set_duration_since_epoch}, uunit::{Gs, MetersPerSecond2, WithUnits}, Radio, Sirin};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::{Channel, TrySendError}, pubsub::{Publisher, Subscriber}};
 use sirin_shared::{mode::SirinMode, physics::approx_pressure_altitude, time::AbsoluteTimeReference};
 use sirin::song::SongDiscriminant;
@@ -57,36 +57,22 @@ async fn main_task(sirin: &'static mut Sirin) -> Result<(), SirinError> {
     let mut i: u32 = 0;
     let mut last_measurement_time: Option<Instant> = None;
 
-    let airbrakes_pwm_1_pin = PwmPin::new_ch4(&mut sirin.gpio.p5, OutputType::PushPull);
-    let mut airbrakes_pwm_1_peri = unsafe {
-        SimplePwm::new(
-            Peripherals::steal().TIM3,
-            None,
-            None,
-            None,
-            Some(airbrakes_pwm_1_pin),
-            khz(10),
-            Default::default()
-        )
+    let mut p = unsafe {
+        Peripherals::steal()
     };
-    let mut airbrakes_pwm_1 = airbrakes_pwm_1_peri.ch4();
 
-    let airbrakes_pwm_2_pin = PwmPin::new_ch3(&mut sirin.gpio.p4, OutputType::PushPull);
-    let mut airbrakes_pwm_2_peri = unsafe {
-        SimplePwm::new(
-            Peripherals::steal().TIM3,
-            None,
-            None,
-            Some(airbrakes_pwm_2_pin),
-            None,
-            khz(10),
-            Default::default()
-        )
-    };
-    let mut airbrakes_pwm_2 = airbrakes_pwm_2_peri.ch3();
-
-    let encoder_channel_1 = gpio::Input::new(&mut sirin.gpio.p6, gpio::Pull::Up);
-    let encoder_channel_2 = gpio::Input::new(&mut sirin.gpio.p7, gpio::Pull::Up);
+    let mut adc = Adc::new(p.ADC3);
+    
+    adc.set_sample_time(SampleTime::CYCLES32_5);
+    
+    let mut vrefint_channel = adc.enable_vrefint();
+    loop {
+        let vrefint = adc.blocking_read(&mut vrefint_channel);
+        info!("vrefint: {}", vrefint);
+        let measured = adc.blocking_read(&mut p.PC0);
+        info!("measured: {}", measured);
+        Timer::after_millis(500).await;
+    }
 
     sirin.spawner.spawn(radio_io_task(&sirin.config, &mut sirin.radio)).unwrap();
     sirin.spawner.spawn(usb_input_task(&mut sirin.usb.read_ep)).unwrap();
@@ -99,12 +85,6 @@ async fn main_task(sirin: &'static mut Sirin) -> Result<(), SirinError> {
     let mut mode = SirinMode::Standby;
 
     let initial_altitude = approx_pressure_altitude(sirin.baro.read().await?.pressure.convert());
-
-    let qem: [i8; 16] = [0,-1,1,2,1,0,2,-1,-1,2,0,1,2,1,-1,0];
-    let old: u8 = 0;
-    let new: u8 = 0;
-    // encoder position (unknown units)
-    let encoder_position: i32 = 0;
 
     let mut flash = Mutex::new(&mut sirin.flash);
     sirin.spawner.spawn(flash_io_task(unsafe {
@@ -218,17 +198,6 @@ async fn main_task(sirin: &'static mut Sirin) -> Result<(), SirinError> {
             &mut sirin.high_g_imu
         ).await;
 
-        old = new;
-        new = encoder_channel_1.get_level() * 2 + encoder_channel_2.get_level();
-        let out = qem[old*4+new];
-
-        if out == 2 {
-            println!("unexpected encoder reading");
-        }
-        else {
-            encoder_position += out;
-        }
-
         if let Ok(pressure) = sirin.data.baro.pressure {
             let measured_altitude = approx_pressure_altitude(pressure.convert());
 
@@ -287,9 +256,26 @@ async fn main_task(sirin: &'static mut Sirin) -> Result<(), SirinError> {
     }
 }
 
-async fn airbrakes(
-    mut pwm: SimplePwmChannel<'_, impl GeneralInstance4Channel>,
-    altitude: Meters<f64>
+// TODO: airbreaks
+/*#[task]
+async fn kalman(
+    mut event_sub: Subscriber<'static, CriticalSectionRawMutex, Event, 100, 4, 4>
 ) {
-    pwm.set_duty_cycle_fraction(1, 2);
-}
+    loop {
+        let event = event_sub.next_message_pure().await;
+
+        match event {
+            Event::Measurement(measurement) => {
+                match measurement {
+                    Measurement::Baro(bmp3_readout) => todo!(),
+                    Measurement::ImuAccel(accel) => todo!(),
+                    Measurement::ImuAngularVel(angular_vel) => todo!(),
+                }
+            },
+        }
+
+        // Example: call a C function from sirin-c Rust crate
+        // Edit sirin-c crate and c project to add more functions
+        sirin_c::cmsis_dsp_sin(f32::consts::PI / 2.0);
+    }
+}*/
